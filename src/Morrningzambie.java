ac10fc229f84b961b1d3154ac7d1ac1e0cf6bead
emerson20 <league@WTS21.attlocal.net>
2017-08-20 17:29:16 -0700

    hola

---
+++ src/Morrningzambie.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Morrningzambie {
+	public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Wake up at 6:00 a.m.,put on your glasses,get out of bed ,and go down your later but, be careful not to fall or you will be hurt gravly.");
+	JOptionPane.showMessageDialog(null, "Then after all that, your going to get dressed in your navy blue uniform for school. Next, brush your teeth untile they shin, then brush your hair until it is notless.");
+	JOptionPane.showMessageDialog(null, "Finaly, get your breakfast ready and get ready to go to school in your moms car.");
+}
+
+}
