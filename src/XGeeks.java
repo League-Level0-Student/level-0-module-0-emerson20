1f3738211efd058ee1eb9c5ba93bf7146156e540
emerson20 <league@WTS21.attlocal.net>
2017-10-01 17:28:58 -0700

    XGeeks in progress

---
+++ src/XGeeks.java
@@ -0,0 +1,24 @@
+// Copyright Wintriss Technical Schools 2014
+import javax.swing.JOptionPane;
+
+/*
+ * Everyone has a superpower. Mine is writing recipes. This program will store
+ * the superpowers of all the people in the classroom. E.g. When I type "June",
+ * your program should say "June's superpower is writing recipes".
+ */
+public class XGeeks {
+public static void main(String[] args) {
+
+		// 1. Save the superpower for each person in a variable.
+	String epower = "magical abilities";
+	String gpower = "tennis";
+	String spower = "eating";
+	String ppower = "flexibility";
+		// 2. Ask the user to enter a name. Store their answer in a variable.
+String pick = JOptionPane.showInputDialog("Pick one out of these four names:Emerson, Grace, Sofia, Priyankaa.");
+		// 3. Show the superpower in a pop-up, depending on the name entered. 
+
+	}
+}
+
+
