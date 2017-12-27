e6776838c2fffb9458636c0775a811bbe5e88a1c
nikhilrajgopal <league@WTS-iMac-11.attlocal.net>
2017-12-27 12:23:28 -0800

    hobbies

---
+++ src/Hobies.java
@@ -0,0 +1,67 @@
+import javax.swing.JOptionPane;
+
+public class Hobies {public static void main(String[] args) {
+	String hob = JOptionPane.showInputDialog("What is your favorite book out of these choses: Harry Potter, The Girl Who Drank the Moon, or the Keeper of the Lost Cities?");
+	if (hob.equals("Harry Potter")) {
+		JOptionPane.showMessageDialog(null, "Harry Potter and Voldemort are conected because part of Voldemorts soul is in Harry Potter which means Harry is one of Voldemorts horcruxes.");
+	}
+	if (hob.equals("The Girl Who Drank the Moon")) {
+		JOptionPane.showMessageDialog(null, "The head sister is actully a which by the name Sarrow Eater.");
+	}
+	if (hob.equals("The Keeper of the Lost Cities")) {
+		JOptionPane.showMessageDialog(null, "The main character Sophie Foster's mind is unbrakeable because (or at least Alden thinks)she was not born, but made.");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+}
